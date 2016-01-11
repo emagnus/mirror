3e3adc7297afb3cbe1f37dbf61c4560e417c7af7
Magnus Westergaard <westerga@gmail.com>
2016-01-11 22:02:38 +0100

    Color test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello pi!');
+  res.send('<html><head><title>PI!</title></head><body style="background-color: black; color: white;">Hello, PI!</body></html>');
 });
 
 app.listen(3000);
