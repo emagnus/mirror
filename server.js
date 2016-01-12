6daff2aa5cd1b77b0911c6619e06f2eb1c13f6ea
Tine Kleivane <kleivane@gmail.com>
2016-01-12 22:27:49 +0100

    Vis formattert klokkeslett

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var moment = require('moment');
 
 var app = express();
 
@@ -7,7 +8,8 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+	console.log(moment().format())
+    res.render('home', {time: moment().format('HH:mm:ss')});
 });
 
 app.listen(3000);
