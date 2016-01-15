3b85fa9226776f87aa765ed2874d501df7b66671
Tine Kleivane <kleivane@gmail.com>
2016-01-15 20:13:33 +0100

    Fjern kommentar

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var moment = require('moment');
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-	console.log(moment().format())
-    res.render('home', {time: moment().format('HH:mm:ss')});
+app.get('/', function(req, res) {
+	res.render('home', {
+		time: moment().format('HH:mm:ss')
+	});
 });
 
 app.listen(3000);
