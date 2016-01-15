787a9f68b250b20f64ec3e4a3a88e40dd194bfb8
Tine Kleivane <kleivane@gmail.com>
2016-01-15 21:11:01 +0100

    Refaktorer til chaining

--- ruter.js
+++ ruter.js
@@ -1,6 +1,7 @@
 var rp = require('request-promise'),
 	_ = require('lodash'),
-	moment = require('moment');
+	moment = require('moment'),
+	TIME_FORMAT = 'HH:mm:ss';
 
 moment.locale('nb');
 
@@ -8,13 +9,23 @@ rp({
 	uri: 'http://reisapi.ruter.no/Favourites/GetFavourites?favouritesRequest=3010625-18-Ljabru',
 	json: true //3010625-18-Ljabru Stopid1-lineid1-destinationtext1
 }).then(function(json) {
-	var ljabru = _.first(json);
-	var firstStop = _.first(ljabru.MonitoredStopVisits);
-	expectedArrivalTime = firstStop.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime;
-	var arrival = moment(expectedArrivalTime);
-	console.log(moment().format('HH:mm:ss'));
-	console.log(arrival.format('HH:mm:ss'));
-	console.log(moment().to(arrival));
+	var arrivals = _(json)
+		.map(function(line) {
+			return _.first(line.MonitoredStopVisits)
+		})
+		.map(function(visit) {
+			return visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime;
+		})
+		.map(function(expectedArrivalTime) {
+			console.log(expectedArrivalTime)
+			return moment(expectedArrivalTime);
+		})
+		.value();
+	var firstArrival = _.first(arrivals);
+	var now = moment();
+	console.log('now', now.format(TIME_FORMAT))
+	console.log('firstArrival', firstArrival.format(TIME_FORMAT));
+	console.log('går: ', now.to(firstArrival));
 }).catch(function(err) {
 	console.log(err)
 });
