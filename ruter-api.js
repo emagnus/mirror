12f7aabecd276bcb56db9dd11d038ee5fea90976
Tine Kleivane <kleivane@gmail.com>
2016-01-15 22:37:12 +0100

    Legg til linje 19

--- ruter-api.js
+++ ruter-api.js
@@ -1,12 +1,12 @@
 var rp = require('request-promise'),
 	_ = require('lodash'),
 	moment = require('moment');
-	
+
 moment.locale('nb');
 
 module.exports = function() {
 	return rp({
-		uri: 'http://reisapi.ruter.no/Favourites/GetFavourites?favouritesRequest=3010625-18-Ljabru,3010624-37-Helsfyr',
+		uri: 'http://reisapi.ruter.no/Favourites/GetFavourites?favouritesRequest=3010625-18-Ljabru,3010625-19-Ljabru,3010624-37-Helsfyr',
 		json: true //3010625-18-Ljabru Stopid1-lineid1-destinationtext1
 	}).then(function(json) {
 		console.log('ruter called successfully')
