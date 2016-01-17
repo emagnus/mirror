61ba84cf4864b7ed45026c369182b5c07acd6c8e
Magnus Westergaard <westerga@gmail.com>
2016-01-17 20:15:36 +0100

    Watching for file changes, take 2.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export DISPLAY=:0
-forever start -l mirrorConsole.log -a --uid "mirrorServer" -w --watchDirectory . server.js
+forever start -l mirrorConsole.log -a --uid "mirrorServer" -w server.js
